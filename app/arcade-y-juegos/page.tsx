df0481c8594a4d1f1bde7b03afb0e7ad3000c7cd
Antonio Lozano <Antonio Lozano>
2026-04-19 17:41:53 +0200

    fix: rename dynamic import to nextDynamic to avoid conflict

--- app/arcade-y-juegos/page.tsx
+++ app/arcade-y-juegos/page.tsx
@@ -3,8 +3,8 @@
 export const dynamic = 'force-dynamic'
 import { Navbar1 } from "@/components/arcade/Navbar1"
 import { Header60 } from "@/components/arcade/Header60"
-import dynamic from "next/dynamic"
-const Layout513 = dynamic(() => import("@/components/arcade/Layout513").then(m => ({ default: m.Layout513 })), { ssr: false })
+import nextDynamic from "next/dynamic"
+const Layout513 = nextDynamic(() => import("@/components/arcade/Layout513").then(m => ({ default: m.Layout513 })), { ssr: false })
 import { Layout384 } from "@/components/arcade/Layout384"
 import { Cta51 } from "@/components/arcade/Cta51"
 import { Footer5 } from "@/components/arcade/Footer5"
--- app/bolera-y-precios/page.tsx
+++ app/bolera-y-precios/page.tsx
@@ -4,8 +4,8 @@ export const dynamic = 'force-dynamic'
 import { Navbar1 } from "@/components/bolera/Navbar1"
 import { Header60 } from "@/components/bolera/Header60"
 import { Pricing15 } from "@/components/bolera/Pricing15"
-import dynamic from "next/dynamic"
-const Layout514 = dynamic(() => import("@/components/bolera/Layout514").then(m => ({ default: m.Layout514 })), { ssr: false })
+import nextDynamic from "next/dynamic"
+const Layout514 = nextDynamic(() => import("@/components/bolera/Layout514").then(m => ({ default: m.Layout514 })), { ssr: false })
 import { Cta51 } from "@/components/bolera/Cta51"
 import { Testimonial6 } from "@/components/bolera/Testimonial6"
 import { Footer5 } from "@/components/bolera/Footer5"
