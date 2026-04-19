4da98f38ce189fc731a7291c952d7bf6823be26e
Antonio Lozano <Antonio Lozano>
2026-04-19 17:37:27 +0200

    fix: dynamic ssr:false for Layout513 and Layout514

--- app/arcade-y-juegos/page.tsx
+++ app/arcade-y-juegos/page.tsx
@@ -3,7 +3,8 @@
 export const dynamic = 'force-dynamic'
 import { Navbar1 } from "@/components/arcade/Navbar1"
 import { Header60 } from "@/components/arcade/Header60"
-import { Layout513 } from "@/components/arcade/Layout513"
+import dynamic from "next/dynamic"
+const Layout513 = dynamic(() => import("@/components/arcade/Layout513").then(m => ({ default: m.Layout513 })), { ssr: false })
 import { Layout384 } from "@/components/arcade/Layout384"
 import { Cta51 } from "@/components/arcade/Cta51"
 import { Footer5 } from "@/components/arcade/Footer5"
